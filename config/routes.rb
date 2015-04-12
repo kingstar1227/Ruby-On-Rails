d02042d0a85e027a8a4f5034746381d1ff22a77f
kingstar1227 <tedd8785@gmail.com>
2015-04-12 12:06:20 +0200

    State after running rails g resource Quiz

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :quizzes
+
   resources :units
   resources :vocables, only: [:create,:edit,:update, :destroy]
   post 'questions' => 'questions#create'
