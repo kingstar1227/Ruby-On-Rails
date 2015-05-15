8172a3a1c65bb2ebbf9d00563b44178e5b2e8dfe
kingstar1227 <tedd8785@gmail.com>
2015-05-15 10:48:13 +0200

    Added Config for capybara

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,7 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'capybara/rails'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
@@ -16,4 +17,6 @@ end
 class ActionDispatch::IntegrationTest
   # Make the Capybara DSL available in all integration tests
   include Capybara::DSL
+  Capybara.reset_sessions!
+  Capybara.use_default_driver
 end
