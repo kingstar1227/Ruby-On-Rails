3d7ee4653aae40c6dcb34fc97c12b10cd80a49ef
kingstar1227 <tedd8785@gmail.com>
2015-05-15 14:24:30 +0200

    Moved Unit_subscription controller testing in the /test/controllers folder

--- test/controllers/unit_subscriptions_controller_test.rb
+++ test/controllers/unit_subscriptions_controller_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class UnitSubscriptionsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @unit = Unit.create!(name: 'Integration_test')
+    @user = User.create!(email: "test@test.de", password: "testtest", password_confirmation: "testtest")
+    @unit_subscription = UnitSubscription.create(unit_id: @unit.id, user_id: @user.id)
+  end
+
+  test 'creates unit_subscription with valid data' do
+    post :create, { score: 1, unit_id: 1, user_id: 1}
+    assert_equal 302, response.status
+  end
+
+  test 'shows unit_subscription' do
+    get :show, id: @unit_subscription.id
+    assert_equal 200, response.status
+  end
+
 end
